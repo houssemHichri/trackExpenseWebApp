48c7d192038de7a0f48cc30779221247019b666d
HICHRI <hh@goat.money>
2018-07-22 23:41:12 +0100

    edit db credentials

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 // mongo DB conf
-mongoose.connect('mongodb://db_user:db_pass2018@ds145951.mlab.com:45951/titus_db');
+mongoose.connect('mongodb://db_user:db_pass2018@ds145981.mlab.com:45981/titus_db');
 // const Cat = mongoose.model('Cat', { name: String });
 // const kitty = new Cat({ name: 'Zildjian' });
 // kitty.save().then(() => console.log('meow'));
